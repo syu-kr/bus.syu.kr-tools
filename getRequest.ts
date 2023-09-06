4f1582b6caa7044d7b48036fadc85568ba78c106
dev-ys-36 <tkddbs7631@naver.com>
2023-09-06 17:48:17 +0900

    added bus distance & checker

--- getRequest.ts
+++ getRequest.ts
@@ -46,6 +46,7 @@ interface BusStatusNew {
       status: string
       routeid: number
       updatetime: number
+      busstop: string
     },
   ]
 }
@@ -68,6 +69,101 @@ const getTime = (): string => {
 
 const getPrefix = (): string => {
   return '[' + getDate() + ' ' + getTime() + ']'
+}
+
+const getDistance = (pos1: any, pos2: any): any => {
+  var lat1 = pos1['lat']
+  var lng1 = pos1['lon']
+  var lat2 = pos2['lat']
+  var lng2 = pos2['lon']
+
+  var R = 6371
+  var dLat = deg2rad(lat2 - lat1)
+  var dLon = deg2rad(lng2 - lng1)
+  var a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2)
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+  var d = R * c
+  var convert = +d.toFixed(5) * 1000 // convert km to m
+  return convert
+}
+
+const deg2rad = (deg: any): any => {
+  return deg * (Math.PI / 180)
+}
+
+const busStop: any = {
+  '삼육대': {
+    lat: 37.64349,
+    lon: 127.105649,
+  },
+  '화랑대역': {
+    lat: 37.619984,
+    lon: 127.085462,
+  },
+  '태릉입구역': {
+    lat: 37.617969,
+    lon: 127.077054,
+  },
+  '석계역': {
+    lat: 37.615088,
+    lon: 127.067217,
+  },
+  '별내역': {
+    lat: 37.642239,
+    lon: 127.126747,
+  },
+  '월릉교': {
+    lat: 37.6162,
+    lon: 127.07129,
+  },
+  '경춘선숲길, 토끼굴앞': {
+    lat: 37.6162,
+    lon: 127.07129,
+  },
+  '봉화산역': {
+    lat: 37.61745,
+    lon: 127.091115,
+  },
+  '두산대림아파트': {
+    lat: 37.61895,
+    lon: 127.087818,
+  },
+  '화랑대사거리': {
+    lat: 37.62158,
+    lon: 127.087228,
+  },
+  '경춘선숲길': {
+    lat: 37.623846,
+    lon: 127.090894,
+  },
+  '서울여대, 육군사관학교': {
+    lat: 37.626043,
+    lon: 127.09468,
+  },
+  '태릉': {
+    lat: 37.630241,
+    lon: 127.098309,
+  },
+  '태릉선수촌': {
+    lat: 37.633423,
+    lon: 127.103218,
+  },
+  '삼육대 정문': {
+    lat: 37.638211,
+    lon: 127.107558,
+  },
+}
+
+const nearBusStop = (pos: any): any => {
+  let meter: any = {}
+  for (let index in busStop) {
+    meter[index] = getDistance(pos, busStop[index])
+  }
+  const sorted = Object.fromEntries(Object.entries(meter).sort((a: any, b: any) => a[1] - b[1]))
+  //console.log(sorted)
+  return Object.keys(sorted)[0]
 }
 
 const getResponse = async (): Promise<void> => {
@@ -93,11 +189,16 @@ const getResponse = async (): Promise<void> => {
   }
 
   const rawJson: BusStatusRaw = JSON.parse(JSON.stringify(await response.json()))
-  const newJson: Partial<BusStatusNew> = {}
+  //const newJson: Partial<BusStatusNew> = {}
+  const newJson: any = {}
 
   newJson.time = getDate() + ' ' + getTime()
   newJson.returnCode = rawJson?.returnCode
-  newJson.data = rawJson?.data
+  // newJson.data = rawJson?.data
+  newJson.data = rawJson?.data?.map((obj: any) => {
+    obj.busstop = nearBusStop(obj)
+    return obj
+  })
 
   fs.writeFile(__dirname + '/api.json', JSON.stringify(newJson, null, 2), (err) => {
     if (err) {
@@ -105,7 +206,74 @@ const getResponse = async (): Promise<void> => {
     }
   })
 
-  let busNumbers = newJson.data?.map((element) => {
+  const monitorRaw: any = JSON.parse(fs.readFileSync(__dirname + '/monitor.json', 'utf8'))
+
+  for (let element of newJson.data) {
+    if (monitorRaw[element.name] == undefined) {
+      monitorRaw[element.name] = []
+    }
+
+    let distance = getDistance(busStop[element.busstop], {'lat': element.lat, 'lon': element.lon})
+
+    // if (
+    //   element.name != '석계역' &&
+    //   element.name != '태릉입구역' &&
+    //   element.name != '화랑대역' &&
+    //   element.name != '삼육대'
+    // ) {
+    //   continue
+    // }
+
+    let monitorNew = {
+      // 'lat': element.lat,
+      // 'lon': element.lon,
+      'busstop': element.busstop,
+      'time': getDate() + ' ' + getTime(),
+      'distance': distance,
+    }
+
+    if (element.status == '0') {
+      continue
+    }
+
+    // if (distance >= 40) {
+    //   continue
+    // }
+
+    // let lastRaw = monitorRaw[element.name].slice(-1)[0]
+    // if (lastRaw != undefined) {
+    //   if (lastRaw.busstop == element.busstop) {
+    //     if (lastRaw.distance < distance) { // 50 < 60
+    //       Array.prototype.pop.call(monitorRaw[element.name])
+    //       console.log(monitorNew)
+    //       continue
+    //     }
+    //   }
+    // }
+
+    console.log(monitorNew)
+
+    if (distance >= 50) {
+      continue
+    }
+
+    let lastRaw = monitorRaw[element.name].slice(-1)[0]
+    if (lastRaw != undefined) {
+      if (lastRaw.busstop == element.busstop) {
+        continue
+      }
+    }
+
+    Array.prototype.push.call(monitorRaw[element.name], monitorNew)
+  }
+
+  fs.writeFile(__dirname + '/monitor.json', JSON.stringify(monitorRaw, null, 2), (err) => {
+    if (err) {
+      console.log(err)
+    }
+  })
+
+  let busNumbers = newJson.data?.map((element: any) => {
     return element.name
   })
 
